7850e7c8d5c069b4d526e2fcb916fe37c46e9151
PeterChen <sanpeter992000@gmail.com>
2021-01-17 18:52:44 +0800

    added comment

--- flask_blog.py
+++ flask_blog.py
@@ -7,4 +7,6 @@ def home():
 
 # 下面這個condition為true的情況為，直接執行這個py檔
 if __name__ == '__main__':
+    # debug設為True時，直接在本機執行，python程式碼修改時，會自動reload，
+    # 但debug設為False時，python程式碼修改時，需要重啟web server，才會reload changes
     app.run(debug=True)
