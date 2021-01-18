6d90638a9810e93707f2ef74dd9a0aa880dcab59
Peter Chen <peter.kt.chen@mail.foxconn.com>
2021-01-18 14:14:54 +0800

    use jinja2 syntax in html

--- flask_blog.py
+++ flask_blog.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+            {
+                'author': 'Corey Schafer',
+                'title': 'Blog Post 1',
+                'content': 'First post content',
+                'date_posted': 'April 20, 2018'
+            },
+            {
+                'author': 'Jane Doe',
+                'title': 'Blog Post 2',
+                'content': 'Second post content',
+                'date_posted': 'April 21, 2018'
+            }
+        ]
+
 # 可以針對同一個function設置不同的route
 @app.route("/")
 @app.route("/home")
 def home():
     # 會直接去找templates資料夾底下的檔案
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
