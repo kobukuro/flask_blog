b5ca49404135f7bdc28f44a563815f676a05ee00
Peter Chen <peter.kt.chen@mail.foxconn.com>
2021-01-21 14:07:42 +0800

    added models in flask_blog.py

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 '''
@@ -10,6 +12,15 @@ python
 >>>secrets.token_hex(16)
 '''
 app.config['SECRET_KEY'] = 'e29ed5b83e8b533582d7a3f1f29d3e7f'
+# 「三個/」代表與此py檔的相對路徑
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+'''
+create db in terminal:
+python
+>>from flask_blog import db
+>>db.create_all()
+'''
+db = SQLAlchemy(app)
 
 posts = [
             {
@@ -25,6 +36,30 @@ posts = [
                 'date_posted': 'April 21, 2018'
             }
         ]
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # 因為會hash過，所以最後image_file固定會是長度20，password固定會是長度60
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # relationship第一個參數為Post，首字母大寫，因為指的是Post class
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    # ForeignKey參數裡的user為小寫，因為指的是table名稱（class轉成table，名稱會變成全小寫）
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 # 可以針對同一個function設置不同的route
 @app.route("/")
